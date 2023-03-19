57baef21611247634be19dfabeceb51c6173bd58
devoperationss <110918922+devoperationss@users.noreply.github.com>
2023-03-19 01:46:03 +0000

    updating all the source files

---
+++ awss3.sh
@@ -0,0 +1,12 @@
+## Create s3 bucket
+
+aws s3api create-bucket --bucket mys318032023 --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+
+## Simple test text file
+echo "Hello, World!" > test.txt
+## Uploading test text file
+aws s3 cp test.txt s3://mys318032023/test.txt
+
+## Checking file in Bucket
+aws s3 ls s3://mys318032023/
+
